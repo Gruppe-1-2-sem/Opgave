cf76f87cb2f2cea4e6b5cfaddc9a964a2f431b9a
nikkynice <74882993+nikkynice@users.noreply.github.com>
2021-03-05 11:02:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+<script>
+        var i = 0; 			// Start Point
+        var images = [];	// Images Array
+        var time = 3000;	// Time Between Switch
+
+        // Image List
+        images[0] = "http://lorempixel.com/400/200/animals";
+        images[1] = "http://lorempixel.com/400/200/sports";
+        images[2] = "http://lorempixel.com/400/200/food";
+        images[3] = "http://lorempixel.com/400/200/people";
+
+    </script>
